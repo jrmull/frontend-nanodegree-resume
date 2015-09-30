ff65a189f00f76c393592c1716e71a3c61a0dc47
Jason Mull <jrmull@gmail.com>
2015-09-30 19:46:04 -0400

    feat: use JQuery to begin building out the bio section

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,18 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var logFailedRequest = function(jqxhr, textStatus, error) {
+  var err = textStatus + ", " + error;
+  console.log("Request Failed: " + err);
+};
+
+var loadBio = function(bioObj, textStatus, jqxhr) {
+  document.getElementById('header').style.display = 'flex';
+  var formattedName = HTMLheaderName.replace("%data%", bioObj.bio.name);
+  $("#header").prepend(formattedName);
+};
+
+var loadData = function() {
+  //bio
+  $.getJSON("/data/bio.json", loadBio)
+    .fail(logFailedRequest);
+};
+
+$(loadData);
